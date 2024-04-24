b99090b92e7aa60c8a470404f3b9012a0eb0fee7
Ryan Saffel <rks023@email.latech.edu>
2024-04-23 20:52:06 -0500

    Basic Randomized Tile Generation Running

---
+++ Assets/Ryan/Scripts/RandomTileGen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTileGen : MonoBehaviour
+{
+    // List of prefabs to choose from
+    public List<GameObject> prefabsList = new List<GameObject>();
+
+    // Number of prefabs to instantiate
+    public int numberOfPrefabsToInstantiate = 5;
+
+    // Initial position to start spawning prefabs
+    public Vector3 initialSpawnPosition = Vector3.zero;
+
+    // Distance between each prefab in the X direction
+    public float distanceBetweenPrefabs = 5f;
+
+    void Start()
+    {
+        Vector3 spawnPosition = initialSpawnPosition;
+
+        // Instantiate prefabs sequentially with the specified distance between them
+        for (int i = 0; i < Mathf.Min(numberOfPrefabsToInstantiate, prefabsList.Count); i++)
+        {
+            GameObject prefab = prefabsList[Random.Range(0, prefabsList.Count)];
+            
+            // Instantiate the prefab at the calculated spawn position
+            GameObject newObject = Instantiate(prefab, spawnPosition, Quaternion.identity, transform);
+
+            // Update the spawn position for the next prefab
+            spawnPosition.x += distanceBetweenPrefabs;
+        }
+    }
+}
