0e4c3e9c0a4604097f62d9b0250d2183c4ac3427
Madeline Ballew <mnb020@latech.edu>
2024-04-23 20:22:04 -0500

    add start screen and walking audio

---
+++ Assets/Madeline/startScreen/startScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene"); //idk what the main game scene is called, need to replace with that
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit button pushed");
+
+        Application.Quit();
+
+        // uncomment this to also make it quit in editor - testing
+        // UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
