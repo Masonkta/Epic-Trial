88a9f899c6e0482b84615368be8b8634d2bb46d0
Masonkta <88817850+Masonkta@users.noreply.github.com>
2024-04-30 19:35:25 -0500

    changes

--- Assets/Mason/Scripts/Enemy.cs
+++ Assets/Mason/Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public enum EnemyType
 {
@@ -16,14 +17,18 @@ public class Enemy : MonoBehaviour
 {
     public int EnemyHealth;
     public int EnemyDefence;
+    public int EnemyDamage;
     HighScoreTest hs;
     public EnemyType Etype;
 
     public static int enemiesKilled = 0;
     public static int scoreMultiplier = 1;
-    public int DoubleScore = 2; 
-    public int TripleScore = 5;
-    public float timeFrame = 30f;
+    public int Double = 2; 
+    public int Triple = 5;
+    float startTime;
+    bool timerStarted = false;
+    float timeFrame = 10f;
+    float timeElapsed = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -52,41 +57,107 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+           if (EnemyType.Weak == Etype)
+            {
+                EnemyDamage = 2;
+                PlayerB player = collision.gameObject.GetComponent<PlayerB>();
+                player.PlayerHealth -= EnemyDamage;
+                Debug.Log(Etype.ToString() + " Dealt " + EnemyDamage + " damage to the Player");
+            }
 
+            if (EnemyType.Medium == Etype)
+            {
+                EnemyDamage = 10;
+                PlayerB player = collision.gameObject.GetComponent<PlayerB>();
+                player.PlayerHealth -= EnemyDamage;
+                Debug.Log(Etype.ToString() + " Dealt " + EnemyDamage + " damage to the Player");
+            }
 
+            if (EnemyType.Heavy == Etype)
+            {
+                EnemyDamage = 20;
+                PlayerB player = collision.gameObject.GetComponent<PlayerB>();
+                player.PlayerHealth -= EnemyDamage;
+                Debug.Log(Etype.ToString() + " Dealt " + EnemyDamage + " damage to the Player");
+            }
+
+            if (EnemyType.Boss == Etype)
+            {
+                EnemyDamage = 30;
+                PlayerB player = collision.gameObject.GetComponent<PlayerB>();
+                player.PlayerHealth -= EnemyDamage;
+                Debug.Log(Etype.ToString() + " Dealt " + EnemyDamage + " damage to the Player");
+            }
+        }
+    }
+
+    public void timer()
+    {
+        startTime = Time.time;
+        timerStarted = true;
+
+    }
     // Update is called once per frame
     void Update()
     {
         if (EnemyHealth <= 0)
         {
-
-            enemiesKilled++;
-
-
-            if (enemiesKilled >= TripleScore)
+            if (!timerStarted)
             {
-                scoreMultiplier = 3;
-                Debug.Log("Mulitplier 3x");
+                timer();
             }
-            else if (enemiesKilled >= DoubleScore)
+            timeElapsed = Time.time - startTime;
+            Debug.Log(timeElapsed);
+
+            if (timeElapsed <= timeFrame)
             {
-                scoreMultiplier = 2;
-                Debug.Log("Mulitplier 2x");
+                enemiesKilled++;
+                Debug.Log(enemiesKilled);
+
+
+                if (enemiesKilled >= Triple)
+                {
+                    scoreMultiplier = 3;
+                    Debug.Log("Mulitplier 3x");
+                }
+                else if (enemiesKilled >= Double)
+                {
+                    scoreMultiplier = 2;
+                    Debug.Log("Mulitplier 2x");
+                }
+                else
+                {
+                    scoreMultiplier = 1;
+                    Debug.Log("Mulitplier 1x");
+
+                }
+
+                int scoreToAdd = GetScore(Etype) * scoreMultiplier;
+                Debug.Log("Gained " + scoreToAdd + " points");
+                hs.score += scoreToAdd;
+
+                Debug.Log("Current Score is " + hs.score);
+                Destroy(gameObject);
             }
-            else
-            {
-                scoreMultiplier = 1;
-                Debug.Log("Mulitplier 1x");
-
-            }
-
-            int scoreToAdd = GetScore(Etype) * scoreMultiplier;
-            Debug.Log("Gained " + scoreToAdd + " points");
-            hs.score += scoreToAdd;
-
-            Debug.Log("Current Score is " + hs.score);
-            Destroy(gameObject);
         }
+        // Check if time frame has elapsed and reset counters
+        if (timerStarted)
+        {
+            Debug.Log("started");
+            Debug.Log(timeElapsed);
+            if (timeElapsed > timeFrame)
+                {
+                    // Reset counters
+                    enemiesKilled = 0;
+                    timerStarted = false;
+                    scoreMultiplier = 1;
+                    Debug.Log("Mulitplier 1x");
+                }
+            }
     }
 
     // Method to get score based on enemy type
--- Assets/Mason/Scripts/Pickup.cs
+++ Assets/Mason/Scripts/Pickup.cs
@@ -1,19 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlayerB : MonoBehaviour
+public class Pickup : MonoBehaviour
 {
-    public float PlayerHealth;
-    public float PlayerDefence;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        PlayerHealth = 100;
-        PlayerDefence = 0;
+        
     }
 
     // Update is called once per frame
