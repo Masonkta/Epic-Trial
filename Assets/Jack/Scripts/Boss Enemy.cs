98467ef29d3b4c65b5e569a9cf2d6cb7fc00ca6e
Masonkta <88817850+Masonkta@users.noreply.github.com>
2024-05-08 15:52:17 -0500

    Boss now can drop from any height and function properly

---
+++ Assets/Jack/Scripts/Boss Enemy.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossEnemy : MonoBehaviour
+{
+    gameHandler gameScript;
+    public NavMeshAgent agent;
+    public GameObject enemyEyes;
+
+    GameObject playerKeyboard;
+
+    GameObject playerController;
+
+    public GameObject Enemy;
+
+    public LayerMask ground, playerArea;
+
+    // Patroling Variables
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    public float attackCoolDown = 1.0f;
+
+    [Header("Animation")]
+    public Animator EnemyAnimator;
+
+
+    // Attack placeholder
+    private bool CanAtt = true;
+
+
+    // Enemy States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        gameScript = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<gameHandler>();
+        playerKeyboard = gameScript.keyboardPlayer;
+        playerController = gameScript.controllerPlayer;
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet){
+            SearchWalkPoint();
+        }
+        if (transform.position.y <= 3)
+        {
+            if (walkPointSet)
+            {
+                agent.SetDestination(walkPoint);
+
+                //  this is a line in the SCENE view that shows its patrol spot
+                Debug.DrawLine(transform.position, walkPoint, Color.yellow);
+            }
+        }
+        
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        // calculates random x and z in range
+        float zPoint = Random.Range(-walkPointRange, walkPointRange);
+        float xPoint = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + xPoint, transform.position.y, transform.position.z + zPoint);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, ground))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void Chase()
+    {
+        if (transform.position.y <= 3)
+        {
+            if (playerKeyboard.activeInHierarchy && playerController.activeInHierarchy)
+            {
+                if (Vector3.Distance(transform.position + Vector3.up, playerKeyboard.transform.position) < Vector3.Distance(transform.position + Vector3.up, playerController.transform.position))
+                {
+                    agent.SetDestination(playerKeyboard.transform.position);
+                }
+                    else
+                    {
+                        agent.SetDestination(playerController.transform.position);
+                    }
+            }
+            if (!playerKeyboard.activeInHierarchy && playerController.activeInHierarchy)
+            {
+                agent.SetDestination(playerController.transform.position);
+            }
+
+            if (playerKeyboard.activeInHierarchy && !playerController.activeInHierarchy)
+            {
+                agent.SetDestination(playerKeyboard.transform.position);
+            }
+        }
+    }
+
+    private void EngagingPlayer()
+    {
+        if (playerInAttackRange && CanAtt)
+        {
+            Attacking();
+        }
+    }
+
+    public void Attacking()
+    {
+        CanAtt = false;
+        EnemyAnimator.SetTrigger("ATTAAACK");
+        StartCoroutine(ResetAtt());
+    }
+
+    IEnumerator ResetAtt()
+    {
+        yield return new WaitForSeconds(attackCoolDown);
+        Chase();
+        EnemyAnimator.SetTrigger("STOOOP");
+        CanAtt = true;
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= 3)
+        {
+            agent.enabled = true;
+        }
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerArea);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerArea);
+        EngagingPlayer();
+
+        if (!playerInSightRange)
+        {
+            enemyEyes.SetActive(false);
+            EnemyAnimator.SetTrigger("RUUUN");
+            Patroling();
+        }
+        if (playerInSightRange)
+        {
+            enemyEyes.SetActive(true);
+            if (CanAtt)
+            {
+                EnemyAnimator.SetTrigger("RUUUN");
+                Chase();
+            }
+            Debug.DrawLine(transform.position, agent.destination, Color.red);
+        }
+    }
+}
