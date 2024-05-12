90513ff066b1c18649def98c463eca09998d9345
Evan Matherne <58615631+CarlRox55@users.noreply.github.com>
2024-05-12 16:17:56 -0500

    safe

--- Assets/handleUITutorial.cs
+++ Assets/handleUITutorial.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class handleUITutorial : MonoBehaviour
 {
+    public gameHandler gameScript;
+    public TextMeshProUGUI playerKCloth;
+    public TextMeshProUGUI playerCCloth;
+    public TextMeshProUGUI playerKWood;
+    public TextMeshProUGUI playerCWood;
+    public TextMeshProUGUI playerKIron;
+    public TextMeshProUGUI playerCIron;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameScript = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<gameHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerKCloth.text = gameScript.clothPieces.ToString();
+        playerCCloth.text = gameScript.clothPieces.ToString();
+        playerKWood.text = gameScript.woodPieces.ToString();
+        playerCWood.text = gameScript.woodPieces.ToString();
+        playerKIron.text = gameScript.ironPieces.ToString();
+        playerCIron.text = gameScript.ironPieces.ToString();
     }
 }
