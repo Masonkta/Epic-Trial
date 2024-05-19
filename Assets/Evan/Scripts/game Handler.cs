928ec64b5f4dc21bb3b66ca3033a57c989a595bb
Ryan Saffel <rks023@email.latech.edu>
2024-05-19 05:25:07 -0500

    Player Respawn and Spectator/Arena Cam Implemented

--- Assets/Evan/Scripts/game Handler.cs
+++ Assets/Evan/Scripts/game Handler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class gameHandler : MonoBehaviour
 {
@@ -51,7 +52,12 @@ public class gameHandler : MonoBehaviour
     [Header("Death Cameras")]
     public GameObject BackUpCamera;
     public GameObject BackUpCamera1;
-
+    public GameObject SpectatorCam;
+    private float respawnTime = 15f;
+    private float respawnTimer;
+    private bool keyboardDead = false;
+    private bool controllerDead = false;
+    private Vector3 respawnPosition = new Vector3(39, 8, -19);
     ////////////////////////////////////////////////////////
 
     void Start()
@@ -61,6 +67,7 @@ public class gameHandler : MonoBehaviour
         if (BackUpCamera) BackUpCamera.SetActive(false);
         if (BackUpCamera) BackUpCamera.SetActive(false);
         if (BackUpCamera1) BackUpCamera1.SetActive(false);
+        if (SpectatorCam) SpectatorCam.SetActive(false);
         // Activate Second Display
         if (!Application.isEditor)
             Display.displays[1].Activate();
@@ -70,6 +77,7 @@ public class gameHandler : MonoBehaviour
             if (hs_trigger.GetComponent<HighScoreTest>())
                 hs = hs_trigger.GetComponent<HighScoreTest>();
         }
+        respawnTimer = respawnTime;
     }
 
     void Update()
@@ -82,14 +90,26 @@ public class gameHandler : MonoBehaviour
 
                 if (keyboardPlayerHealth <= 0)
                 {
+                    respawnTimer -= Time.deltaTime;
                     keyboardPlayer.SetActive(false);
-                    BackUpCamera.SetActive(true);
+                    SpectatorCam.SetActive(true);
+                    keyboardDead = true;
+                    if (respawnTimer <= 0)
+                    {
+                        RespawnPlayers();
+                    }
                 }
 
                 if (controllerPlayerHealth <= 0)
                 {
+                    respawnTimer -= Time.deltaTime;
                     controllerPlayer.SetActive(false);
-                    BackUpCamera1.SetActive(true);
+                    SpectatorCam.SetActive(true);
+                    controllerDead = true;
+                    if (respawnTimer <= 0)
+                    {
+                        RespawnPlayers();
+                    }
                 }
 
                 if (keyboardPlayerHealth <= 0f && controllerPlayerHealth <= 0f)
@@ -247,5 +267,28 @@ public class gameHandler : MonoBehaviour
     public float GetControllerHealth()
     {
         return controllerPlayerHealth;
+    }
+
+    void RespawnPlayers()
+    {
+        if (keyboardDead)
+        {
+            keyboardPlayer.SetActive(true);
+            keyboardPlayer.transform.position = respawnPosition;
+            keyboardPlayerHealth = 50f;
+            keyboardDead = false;
+        }
+
+        if (controllerDead)
+        {
+
+            controllerPlayer.SetActive(true);
+            controllerPlayer.transform.position = respawnPosition;
+            controllerPlayerHealth = 50f;
+            controllerDead = false;
+        }
+
+        SpectatorCam.SetActive(false);
+        respawnTimer = respawnTime;
     }
 }
